f9294305cf36762bc23df38ea78a7348769ccd45
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-07 16:35:38 -0500

    dark and ligth theme

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,9 +1,10 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs"
 
 export default function Home() {
   return (
     <div>
-    This is protected route
+    <UserButton 
+    afterSignOutUrl="/"/>
     </div>
     
   )
