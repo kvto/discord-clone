84943ccfc21e370e79fe4e23a046bd003773a261
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-15 17:38:18 -0500

    navigation sidebar

--- app/api/servers/route.ts
+++ app/api/servers/route.ts
@@ -32,6 +32,8 @@ export async function POST(req: Request){
                 }
             }
         })
+
+        return NextResponse.json(server);
     } catch(error){
         console.log("[SERVERS_POST]", error);
         return new NextResponse("Internal Error", { status: 500 })
