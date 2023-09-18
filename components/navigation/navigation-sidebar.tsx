da76867614a8b8d82d3743f641b40702eb52aa5b
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-18 17:16:11 -0500

    navigation sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -3,6 +3,10 @@ import { redirect } from "next/navigation";
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { NavigationAction } from "./navigation-action";
+import { Separator } from "@/components/ui/separator";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
 
 
 export const
@@ -27,6 +31,23 @@ NavigationSidebar = async () => {
         <div
         className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] bg-[#E3E5E8] py-3">
             <NavigationAction />
+            <Separator 
+            className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"/>
+            
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} 
+                    className="mb-4">
+                        <NavigationItem 
+                        id={server.id}
+                        name={server.name}
+                        imageUrl={server.imageUrl}/>
+                    </div>
+                ))}
+            </ScrollArea>
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                    <ModeToggle />
+            </div>
         </div>
     )
 }
