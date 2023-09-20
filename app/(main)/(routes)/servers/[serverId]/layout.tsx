2d6fba0155e09c05934156fda07b661b841ca622
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-20 17:09:56 -0500

    aaa

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -1,5 +1,8 @@
+import { ServerSidebar } from "@/components/server/server-sidebar";
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const ServerIdLayout = async ({
     children,
@@ -13,9 +16,29 @@ const ServerIdLayout = async ({
     if(!profile){
         return redirectToSignIn();
     }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if(!server) {
+        return redirect("/")
+    }
     return ( 
-        <div>
-            {children}
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+                <ServerSidebar serverId={params.serverId} />
+            </div>
+            <main className="h-full md:pl-60">
+             {children}   
+            </main>
         </div>
      );
 }
