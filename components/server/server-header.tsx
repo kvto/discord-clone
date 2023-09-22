9c6baa5c9bff53110307140d97d6639f82f15d3f
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-09-22 17:17:06 -0500

    channel creation

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,6 +70,7 @@ role
                 {
                     isModerator && (
                         <DropdownMenuItem
+                        onClick={() => onOpen("createChannel")}
                         className="px-3 py-2 text-sm cursor-pointer">
                             Create Channel
                             <PlusCircle className="h-4 w-4 ml-auto" />
